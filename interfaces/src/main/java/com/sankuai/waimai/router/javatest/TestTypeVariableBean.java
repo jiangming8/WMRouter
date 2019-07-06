d7d90b7849c44fb10e03a0a579f2468d589a533a
cl <309167159@qq.com>
2019-07-06 16:42:26 +0800

    添加测试泛型的代码

---
+++ interfaces/src/main/java/com/sankuai/waimai/router/javatest/TestTypeVariableBean.java
@@ -0,0 +1,31 @@
+package com.sankuai.waimai.router.javatest;
+
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+
+/**
+ * 描述:
+ *
+ * 责任人:cl
+ */
+public class TestTypeVariableBean<K extends Number, T> {
+
+    //K有指定了上边界Number
+    K key;
+    //T没有指定上边界，其默认上边界为Object
+    T value;
+
+    public static void main(String[] args){
+        Type[] types = TestTypeVariableBean.class.getTypeParameters();
+        for (Type type : types){
+            TypeVariable t = (TypeVariable) type;
+            int index = t.getBounds().length - 1;
+            //输出上边界
+            System.out.println("--getBounds()-- " + t.getBounds()[index]);
+            //输出名称
+            System.out.println("--getName()--" + t.getName());
+            //输出所在的类的类型
+            System.out.println("--getGenericDeclaration()--" + 														t.getGenericDeclaration());
+        }
+    }
+}
